1389cf045e17710252fa87e2ec312651f53f7c4c
Mehul <mehulverma96602@gmail.com>
2021-10-02 19:08:59 +0530

    2 new files added

---
+++ lib/cardchild1.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class cardchild1 extends StatelessWidget {
+  cardchild1({
+    @required this.Ikon,
+    @required this.colour,
+    @required this.label,
+  });
+  String label;
+  IconData Ikon;
+  Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Ikon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 20,
+            color: colour,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'cardchild1.dart';
+import 'reusableCard.dart';
+
+const Color colour1 = Color(0xFF1D1E33);
 
 class InputPage extends StatefulWidget {
   @override
@@ -14,12 +20,64 @@ class _InputPageState extends State<InputPage> {
           child: Text('BMI CALCULATOR'),
         ),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    cardChild: cardchild1(
+                      Ikon: FontAwesomeIcons.mars,
+                      colour: Color(0xFF8D8E98),
+                      label: 'MALE',
+                    ),
+                    colour: colour1,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardChild: cardchild1(
+                      Ikon: FontAwesomeIcons.venus,
+                      colour: Color(0xFF8D8E98),
+                      label: 'FEMALE',
+                    ),
+                    colour: colour1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: colour1,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: colour1,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: colour1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            height: 80,
+            width: double.infinity,
+            color: Color(0xFFEB1555),
+          )
+        ],
       ),
     );
   }
 }
+
